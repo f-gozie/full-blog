5f369c5fac41a491362836f2e9353f594a1a762c
Agozie Favour <fagozie43@gmail.com>
2021-06-25 23:54:23 +0100

    Chore: Changed project name

--- full_blog/asgi.py
+++ full_blog/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_girls project.
+ASGI config for full_blog project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_girls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'full_blog.settings')
 
 application = get_asgi_application()
--- full_blog/wsgi.py
+++ full_blog/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_girls project.
+WSGI config for full_blog project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_girls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'full_blog.settings')
 
 application = get_wsgi_application()
